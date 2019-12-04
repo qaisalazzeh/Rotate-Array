d4aa51af39372fda9c8841b9a81657ad1ec04b3a
Al-Azzeh <qazzeh@88e9fe67a71a.ant.amazon.com>
2019-12-04 15:19:08 +0200

    slove the first question

---
+++ src/com/app/rotate/array/Solution.java
@@ -0,0 +1,95 @@
+package com.app.rotate.array;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 
+ * @author qazzeh
+ *
+ */
+public class Solution {
+
+	public static int NUMBER_OF_TEST_CASES = 0;
+
+	public static final String INPUT_FILE_PATH = System.getProperty("user.dir")+ "/src/input.txt";
+	public static HashMap<String, Object> map = new HashMap<String, Object>();
+
+	public static void main(String[] args) {
+		try {
+			fetchFileIntoMap();
+			rotateTheArray();
+
+		} catch (IOException e) {
+			// TODO: handle exception
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
+
+	private static void fetchFileIntoMap() throws FileNotFoundException, IOException {
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(INPUT_FILE_PATH));
+
+		Pattern arraysInfoLineP = Pattern.compile("[0-9] [0-9]$");
+		int counter = 0;
+		String line;
+		String tempTestCase = null;
+		while ((line = bufferedReader.readLine()) != null) {
+			if (counter == 0) {
+				NUMBER_OF_TEST_CASES = Integer.valueOf(line);
+				counter++;
+			}
+			Matcher matcher = arraysInfoLineP.matcher(line);
+			if (matcher.find()) {
+				map.put(line, null);
+				tempTestCase = line;
+			} else {
+				if (tempTestCase != null) {
+					map.put(tempTestCase, line);
+					tempTestCase = null;
+				}
+			}
+
+		}
+		bufferedReader.close();
+	}
+
+	private static void rotateTheArray() {
+
+		for (Map.Entry<String, Object> entry : map.entrySet()) {
+			String key = entry.getKey();
+			String[] splited = key.split(" ");
+			int arraySize = Integer.valueOf(splited[0]);
+			int numberSizeOfRotation = Integer.valueOf(splited[1]);
+			int[] array = getArray((String) entry.getValue(), arraySize);
+			int[] tempArrayLast = Arrays.copyOfRange(array, 0, numberSizeOfRotation);
+			int[] tempArrayFirst = Arrays.copyOfRange(array, numberSizeOfRotation, array.length);
+			int tempArrayFirstLenght = tempArrayFirst.length;
+			int tempArrayLastLenght = tempArrayLast.length;
+			int[] result = new int[tempArrayFirstLenght + tempArrayLastLenght];
+			System.arraycopy(tempArrayFirst, 0, result, 0, tempArrayFirstLenght);
+			System.arraycopy(tempArrayLast, 0, result, tempArrayFirstLenght, tempArrayLastLenght);
+			System.out.println(Arrays.toString(result));
+		}
+
+	}
+
+	private static int[] getArray(String value, int arraySize) {
+		int[] array = new int[arraySize];
+		String[] val = value.split(" ");
+		for (int i = 0; i < val.length; ++i) {
+			array[i] = Integer.parseInt(val[i]);
+		}
+		return array;
+
+	}
+
+}
